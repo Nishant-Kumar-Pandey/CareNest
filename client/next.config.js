94ef6a30572c8108609d9bb3703b439fff9228a6
Nishant Kumar Pandey <nishantpandey669@gmail.com>
2026-04-24 17:25:03 +0530

    feat: configure Next.js environment, update project scripts, and add environment variable templates

--- client/next.config.js
+++ client/next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
     remotePatterns: [
       { protocol: 'https', hostname: 'api.dicebear.com' },
       { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'res.cloudinary.com' },
     ],
   },
   env: {
