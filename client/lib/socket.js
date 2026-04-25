b54faa91642302652e9cb3cddfbbc6b65d42fb46
Nishant Kumar Pandey <nishantpandey669@gmail.com>
2026-04-25 22:37:17 +0530

    feat: initialize socket.io client configuration for real-time communication

--- client/lib/socket.js
+++ client/lib/socket.js
@@ -2,6 +2,6 @@ import { io } from 'socket.io-client';
 
 const URL = process.env.NEXT_PUBLIC_API_URL ? process.env.NEXT_PUBLIC_API_URL.replace('/api', '') : 'http://localhost:5000';
 
-export const socket = io(URL, {
-  autoConnect: false // We will manually connect when a user logs in
-});
+export const socket = typeof window !== 'undefined' 
+  ? io(URL, { autoConnect: false }) 
+  : { connected: false, on: () => {}, off: () => {}, emit: () => {}, connect: () => {}, disconnect: () => {} };
